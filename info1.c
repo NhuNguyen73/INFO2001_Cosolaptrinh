0a0f4328034379003112f90e16deabb3d3392084
NhuNguyen73 <BIT230303@st.cmc-u.edu.vn>
2023-10-23 20:38:04 +0700

    Init Project

---
+++ info1.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main() {
+    //Input
+/ #define PI 3.14
+    float R = 3;
+    float mArea;
+    char myChar = 'a';
+   // process
+    mArea = PI * R * R;
+            //output
+    printf( "Hinh tron, ban kinh = %f\n",R);
+    printf( "Dien tich = %f\n", mArea);
+    printf( "Ki tu dang minh hoa la %d\n", myChar); /
+
+
+    return 0;
+}
